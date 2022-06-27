cea64d9bf4adcdd384f8ca671779ee43bee207bc
yongjun <ansdydrn8985@gmail.com>
2022-06-27 17:58:54 +0900

    BoxOfficeApi 추가, index 화면 디자인 수정

---
+++ src/main/java/com/example/movie/Api/BoxOfficeApi.java
@@ -0,0 +1,95 @@
+package com.example.movie.Api;
+
+import com.example.movie.Entity.Movie;
+import com.example.movie.Repository.DailyMovieRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.shaded.json.JSONArray;
+import com.nimbusds.jose.shaded.json.JSONObject;
+import com.nimbusds.jose.shaded.json.parser.JSONParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import kr.or.kobis.kobisopenapi.consumer.rest.KobisOpenAPIRestService;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class BoxOfficeApi {
+
+    private final DailyMovieRepository dailyMovieRepository;
+
+    //발급키
+    String key = "3ab83bf06ebbff20eab7fc271423bc42";
+
+    public void dailyBoxOffice(){
+
+        String dailyResponse = "";
+
+        //전날 박스오피스 조회 ( 오늘 날짜꺼는 안나옴.. )
+        LocalDateTime time = LocalDateTime.now().minusDays(1);
+        String targetDt =  time.format(DateTimeFormatter.ofPattern("yyyMMdd"));
+
+        //ROW 개수
+        String itemPerPage = "10";
+
+        //다양성영화(Y)/상업영화(N)/전체(default)
+        String multiMovieYn = "";
+
+        //한국영화(K)/외국영화(F)/전체(default)
+        String repNationCd = "";
+
+        //상영지역별 코드/전체(default)
+        String wideAreaCd = "";
+
+        try {
+            // KOBIS 오픈 API Rest Client를 통해 호출
+            KobisOpenAPIRestService service = new KobisOpenAPIRestService(key);
+
+            // 일일 박스오피스 서비스 호출 (boolean isJson, String targetDt, String itemPerPage,String multiMovieYn, String repNationCd, String wideAreaCd)
+            dailyResponse = service.getDailyBoxOffice(true, targetDt, itemPerPage, multiMovieYn, repNationCd, wideAreaCd);
+
+            //JSON Parser 객체 생성
+            JSONParser jsonParser = new JSONParser();
+
+            //Parser로 문자열 데이터를 객체로 변환
+            Object obj = jsonParser.parse(dailyResponse);
+
+            //파싱한 obj를 JSONObject 객체로 변환
+            JSONObject jsonObject = (JSONObject) obj;
+
+            //차근차근 parsing하기
+            JSONObject parse_boxOfficeResult = (JSONObject) jsonObject.get("boxOfficeResult");
+
+            //박스오피스 종류
+            String boxofficeType = (String) parse_boxOfficeResult.get("boxofficeType");
+
+            //박스오피스 조회 일자
+            String showRange = (String) parse_boxOfficeResult.get("showRange");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JSONArray parse_dailyBoxOfficeList = (JSONArray) parse_boxOfficeResult.get("dailyBoxOfficeList");
+            for(int i=0; i<parse_dailyBoxOfficeList.size(); i++){
+                JSONObject dailyBoxOffice = (JSONObject) parse_dailyBoxOfficeList.get(i);
+
+                //JSON object -> Java Object(Entity) 변환
+                Movie dailyMovie = objectMapper.readValue(dailyBoxOffice.toString(), Movie.class);
+                String movieCd = dailyMovie.getMovieCd();
+                List<String> movieList = dailyMovieRepository.findAllByMovieCd(movieCd);
+                System.out.println(movieList);
+                //DB저장
+                if(movieList.isEmpty() == true){
+                    dailyMovie.setTargetDt(targetDt);
+                    dailyMovie.setBoxofficeType(boxofficeType);
+                    dailyMovie.setShowRange(showRange);
+                    dailyMovieRepository.save(dailyMovie);
+                }else{
+
+                }
+            }
+        }catch(Exception e){
+            e.getMessage();
+        }
+    }
+}
---
+++ src/main/java/com/example/movie/Entity/Movie.java
@@ -0,0 +1,85 @@
+package com.example.movie.Entity;
+
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name="Movie")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /*박스오피스 종류*/
+    private String boxofficeType;
+
+    /*박스오피스 조회 일자*/
+    private String showRange;
+
+    /*순번*/
+    private String rnum;
+
+    /*해당일자의 박스오피스 순위*/
+    private String rank;
+
+    /*전일대비 순위의 증감분*/
+    private String rankInten;
+
+    /*랭킹에 신규진입여부 “OLD” : 기존 , “NEW” : 신규*/
+    private String rankOldAndNew;
+
+    /*영화의 대표코드*/
+
+    private String movieCd;
+
+    /*영화명(국문)*/
+    private String movieNm;
+
+    /*영화의 개봉일*/
+    private String openDt;
+
+    /*해당일의 매출액*/
+    private String salesAmt;
+
+    /*해당일자 상영작의 매출총액 대비 해당 영화의 매출비율*/
+    private String salesShare;
+
+    /*전일 대비 매출액 증감분*/
+    private String salesInten;
+
+    /*전일 대비 매출액 증감 비율*/
+    private String salesChange;
+
+    /*누적매출액*/
+    private String salesAcc;
+
+    /*해당일의 관객수*/
+    private String audiCnt;
+
+    /*전일 대비 관객수 증감분*/
+    private String audiInten;
+
+    /*전일 대비 관객수 증감 비율*/
+    private String audiChange;
+
+    /*누적관객수*/
+    private String audiAcc;
+
+    /*해당일자에 상영한 스크린수*/
+    private String scrnCnt;
+
+    /*해당일자에 상영된 횟수*/
+    private String showCnt;
+
+    /*DB 저장 일자*/
+    private String targetDt;
+
+    @CreationTimestamp
+    private LocalDateTime modification_date_time;
+
+}
