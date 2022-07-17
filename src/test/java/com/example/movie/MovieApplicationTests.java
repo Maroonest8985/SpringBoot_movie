89129f6d451fe4762c59caea956f84d58aa1a25a
RootBro <81356744+idupgh@users.noreply.github.com>
2022-07-17 15:47:10 +0900

    DB에 알파벳 넣기 성공

--- src/test/java/com/example/movie/MovieApplicationTests.java
+++ src/test/java/com/example/movie/MovieApplicationTests.java
@@ -1,14 +1,18 @@
 package com.example.movie;
 
 import com.example.movie.Api.BoxOfficeApi;
+import com.example.movie.Entity.Cinema;
 import com.example.movie.Entity.Movie;
 import com.example.movie.Repository.DailyMovieRepository;
+import com.example.movie.Service.CinemaService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 import java.util.Scanner;
+
+import static org.apache.coyote.http11.Constants.A;
 
 @SpringBootTest
 class MovieApplicationTests {
@@ -36,5 +40,20 @@ class MovieApplicationTests {
                 System.out.println("{"+a+k+"}],");
             }
         }
+    }
+}
+
+    @Test
+    void member123() {
+        List<Cinema> cinemaList = cinemaService.readAll();
+        System.out.println(cinemaList);
+    }
+
+    @Test
+    void arr() {
+        int asciiValue = 65;
+
+        char convertedChar = (char) asciiValue;
+        System.out.println("---------->"+convertedChar);
     }
 }
