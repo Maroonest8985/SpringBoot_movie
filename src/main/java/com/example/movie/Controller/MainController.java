846c34aed2fe3c6103e40767ab1d98a68acc9bcc
Maroonest8985 <90228118+Maroonest8985@users.noreply.github.com>
2022-07-20 14:43:53 +0900

    스케줄 중간저장-1

--- src/main/java/com/example/movie/Controller/MainController.java
+++ src/main/java/com/example/movie/Controller/MainController.java
@@ -6,8 +6,10 @@ import com.example.movie.Domain.ReserveDTO;
 import com.example.movie.Entity.Member;
 import com.example.movie.Entity.Movie;
 import com.example.movie.Entity.Reserve;
+import com.example.movie.Entity.Schedule;
 import com.example.movie.Repository.DailyMovieRepository;
 import com.example.movie.Repository.MemberRepository;
+import com.example.movie.Repository.ScheduleRepository;
 import com.example.movie.Service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -27,12 +29,16 @@ public class MainController {
     @Autowired
     MemberService memberService;
     @Autowired
+    ScheduleRepository scheduleRepository;
+    @Autowired
     BoxOfficeApi api;
     @GetMapping("/")
     public String getMain(Model model,@RequestParam(required = false,defaultValue = "") String searchMovie){
         api.dailyBoxOffice();
         //List<Movie> movieList = dailyMovieRepository.findAll();
         List<Movie> movieList = dailyMovieRepository.findByMovieNmContaining(searchMovie);
+
+
 
         System.out.println(movieList);
         model.addAttribute("movieList", movieList);
@@ -44,8 +50,14 @@ public class MainController {
     @RequestMapping("/reserve/{idx}")
     public String getReserve(@PathVariable("idx") String movieCd, Model model, HttpSession session){
         Movie movie = dailyMovieRepository.findMovieByMovieCd(movieCd);
+        List<String> dateList = scheduleRepository.getDateByMovieCd(movieCd);
+        //List<String> timeList = scheduleRepository.getTimeByMovieCd();
+        System.out.println(dateList);
         MemberDTO member = (MemberDTO) session.getAttribute("login");
+        model.addAttribute("date", dateList);
+        model.addAttribute("time", timeList);
         model.addAttribute("movie", movie);
+        model.addAttribute("member", member);
         model.addAttribute("reserve", ReserveDTO.builder().build());
         return "reserve";
     }
