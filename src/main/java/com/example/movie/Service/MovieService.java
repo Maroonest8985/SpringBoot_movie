08ddf2f2fca14369f64826efd3bfc3dd78eb20ac
Seong Koo <tjdrn990423@naver.com>
2022-07-20 21:28:22 +0900

    네이버 api 연동까지 완료

---
+++ src/main/java/com/example/movie/Service/MovieService.java
@@ -0,0 +1,19 @@
+package com.example.movie.Service;
+
+import com.example.movie.Api.NaverApi;
+import com.example.movie.Domain.MoviesResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class MovieService {
+    private final NaverApi naverApi;
+
+    @Transactional(readOnly = true)
+    public MoviesResponseDto findByKeyword(String keyword) {
+        return naverApi.requestMovie(keyword);
+    }
+
+}
